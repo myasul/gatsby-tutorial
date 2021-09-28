fcffe26a86c5646e7dd68303fa077bcac39adf40
Matthew Yasul <m.yasul@okrasolar.com>
2021-09-28 22:18:40 +0800

    Add styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import * as footerStyles from './footer.module.scss'
+
 const Footer = () => {
     return (
-        <footer>
-            <p>Site developed by Matt.Y &copy; {new Date().getFullYear()}</p>
+        <footer className={footerStyles.siteFooter}>
+            <div className={footerStyles.container}>
+                <p>
+                    Site developed by Ibaslogic &copy;{" "}
+                    {new Date().getFullYear().toString()}{" "}
+                </p>
+            </div>
         </footer>
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,42 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
+import * as headerStyles from "./header.module.scss"
+
 const Header = () => {
     return (
-        <header>
-            <div>
-                <p>
+        <header className={headerStyles.header}>
+            <div className="{headerStyles.overlay}"></div>
+            <div className={headerStyles.heroContent}>
+                <p className={headerStyles.brand}>
                     <Link to='/'>Matthew Yasul</Link>
                 </p>
-                <p>I write code, lift weights, and drink coffee.</p>
+                <p className={headerStyles.description}>I write code, lift weights, and drink coffee.</p>
             </div>
-            <nav>
-                <ul>
+            <nav className={headerStyles.navContainer}>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link to='/'>Home</Link>
+                        <Link to="/" activeClassName={headerStyles.activeMenuItem}>
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/blog'>Blog</Link>
+                        <Link to="/blog/" activeClassName={headerStyles.activeMenuItem}>
+                            Blog
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/contact'>Contact</Link>
+                        <Link to="/contact/" activeClassName={headerStyles.activeMenuItem}>
+                            Contact
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/about'>About</Link>
+                        <Link to="/about/" activeClassName={headerStyles.activeMenuItem}>
+                            About
+                        </Link>
                     </li>
                 </ul>
+
             </nav>
         </header>
     )
